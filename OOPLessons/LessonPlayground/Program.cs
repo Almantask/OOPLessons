edc79c98bb7dbf99a2872985c275701742a54349
Kaisinel <almantusk@gmail.com>
2019-07-06 21:21:13 +0300

    Lesson #1: Encapsulation, Inheritance, Polymorphism.

---
+++ OOPLessons/LessonPlayground/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LessonPlayground
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Lesson1.Run();
+        }
+    }
+}
